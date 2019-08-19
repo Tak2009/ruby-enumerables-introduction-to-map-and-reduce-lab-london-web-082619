0f3d1c15c8359f096244ade0bc060dbfb5d8e3f8
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-19 20:22:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,3 +38,13 @@ def map_to_square(array)
   end
     new_array
 end
+
+def map_to_square(array)
+  new_array = []
+  i = 0
+    while i < array.length do
+    new_array.push(array[i]**2)
+    i += 1
+  end
+    new_array
+end
