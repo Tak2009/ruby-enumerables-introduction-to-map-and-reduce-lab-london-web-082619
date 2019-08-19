bf11c68165c2d94c9ec9c0e02f89ae72a5d68e62
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-19 20:34:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,11 +40,5 @@ def map_to_square(array)
 end
 
 def reduce_to_total(array)
-  new_array = []
-  i = 0
-    while i < array.length do
-    new_array.push(array[i]**2)
-    i += 1
-  end
-    new_array
+  array.inject {|sum, n| sum + n }
 end
