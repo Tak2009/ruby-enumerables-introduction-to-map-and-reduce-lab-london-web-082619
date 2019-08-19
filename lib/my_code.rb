80100f1a1cd55ffb51da8cab2af787ddd7c4a2b5
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-19 20:21:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,15 +1,25 @@
 def map_to_negativize(array)
-  negative_array = []
+  new_array = []
   i = 0
     while i < array.length do
-    negative_array.push(array[i]*-1)
+    new_array.push(array[i]*-1)
     i += 1
   end
-    negative_array
+    new_array
 end
 
 
 def map_to_no_change(array)
+  new_array = []
+  i = 0
+    while i < array.length do
+    new_array.push(array[i])
+    i += 1
+  end
+    new_array
+end
+
+def map_to_double(array)
   new_array = []
   i = 0
     while i < array.length do
